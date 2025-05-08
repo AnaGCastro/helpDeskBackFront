db6310caf70cb5319d7c57d19971c34f32c915e7
AnaGCastro <testeanateste0@gmail.com>
2025-05-08 17:17:04 -0300

    anotacoes JPA para classes de dominio

--- src/main/java/com/udemy/helpdesk/domain/Tecnico.java
+++ src/main/java/com/udemy/helpdesk/domain/Tecnico.java
@@ -1,18 +1,30 @@
 package com.udemy.helpdesk.domain;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Tecnico extends Pessoa {
+import com.udemy.helpdesk.domain.enums.Perfil;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
+
+@Entity
+public class Tecnico extends Pessoa implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@OneToMany(mappedBy = "tecnico")
 	private List<Chamado> chamados = new ArrayList<>();
 
 	public Tecnico() {
 		super();
+		addPerfil(Perfil.CLIENTE);
 	}
 
 	public Tecnico(Integer id, String nome, String cpf, String email, String senha) {
 		super(id, nome, cpf, email, senha);
+		
+		addPerfil(Perfil.CLIENTE);
 	}
 
 	public List<Chamado> getChamados() {
